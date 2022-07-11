19331a6bc7af89ba97e00dadd8df27d98545ff09
willycoxy <105467011+willycoxy@users.noreply.github.com>
2022-07-11 19:52:43 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,20 @@ fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?inclu
   .then(data => {
     console.log(data.tickers[0])
     document.getElementById('a1').textContent = data.tickers[0].ticker
-    document.getElementById('a2').textContent = data.tickers[0].day.c
-    document.getElementById('a3').textContent = data.tickers[0].todaysChangePerc
+    document.getElementById('a2').textContent = `$ ${data.tickers[0].day.c}`
+    document.getElementById('a3').textContent = `% ${data.tickers[0].todaysChangePerc}`
     document.getElementById('a4').textContent = data.tickers[0].day.v
     document.getElementById('b1').textContent = data.tickers[1].ticker
-    document.getElementById('b2').textContent = data.tickers[1].day.c
-    document.getElementById('b3').textContent = data.tickers[1].todaysChangePerc
+    document.getElementById('b2').textContent = `$ ${data.tickers[1].day.c}`
+    document.getElementById('b3').textContent = `% ${data.tickers[1].todaysChangePerc}`
     document.getElementById('b4').textContent = data.tickers[1].day.v
     document.getElementById('c1').textContent = data.tickers[2].ticker
-    document.getElementById('c2').textContent = data.tickers[2].day.c
-    document.getElementById('c3').textContent = data.tickers[2].todaysChangePerc
+    document.getElementById('c2').textContent = `$ ${data.tickers[2].day.c}`
+    document.getElementById('c3').textContent = `% ${data.tickers[2].todaysChangePerc}`
     document.getElementById('c4').textContent = data.tickers[2].day.v
     document.getElementById('d1').textContent = data.tickers[3].ticker
-    document.getElementById('d2').textContent = data.tickers[3].day.c
-    document.getElementById('d3').textContent = data.tickers[3].todaysChangePerc
+    document.getElementById('d2').textContent = `$ ${data.tickers[3].day.c}`
+    document.getElementById('d3').textContent = `% ${data.tickers[3].todaysChangePerc}`
     document.getElementById('d4').textContent = data.tickers[3].day.v
   })
 
@@ -34,6 +34,14 @@ fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?inclu
 }
 
 marketStatus();
+
+fetch('https://finviz2.p.rapidapi.com/api/v1/news?type=%3CREQUIRED%3E/rapidapi-key=14b035a1f0mshcc430d50a1c6dc4p1056b6jsn7b1435')
+  .then(response => response.json())
+  .then(data=>{
+    console.log(response)
+  })
+
+
 
     
 
