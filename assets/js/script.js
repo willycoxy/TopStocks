ffbeb1d342151636d4ed3de25fdc2c4f14ee8994
Marty <martynolan17@gmail.com>
2022-07-12 11:19:15 -0400

    all index buttons/footer buttons functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 sPButtonEl = document.querySelector(".btn1")
+dowJonesButtonEl = document.querySelector(".btn2");
+nasdaqButtonEl = document.querySelector(".btn3");
 
 function marketStatus () {
     fetch("https://api.polygon.io/v1/marketstatus/now?apiKey=t6vX15DF8kEE3wQ4nnk9aEmGguUiai5H")
@@ -14,24 +16,75 @@ function marketStatus () {
 
 marketStatus();
 
+
 function getsP () {
-    fetch("https://api.polygon.io/v2/aggs/ticker/VOO/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=t6vX15DF8kEE3wQ4nnk9aEmGguUiai5H")
+    fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?tickers=VOO&apiKey=97jcUfkUrkUqlBMzoexxjePUT1lnkBe4")
     .then(response => {
         return response.json();
     })
     .then(data => {
-        console.log(data.results[0].v);
-        const volume = data.results[0].v
-        const high = data.results[0].h
-        const low = data.results[0].l
-        document.querySelector(".volume").innerHTML = `Volume: ${volume}`
-        document.querySelector(".high").innerHTML = `High: ${high}`
-        document.querySelector(".low").innerHTML = `Low: ${low}`
+        console.log(data.tickers[0].day.v);
+        const volume = data.tickers[0].day.v
+        const open = data.tickers[0].day.o
+        const high = data.tickers[0].day.h
+        const low = data.tickers[0].day.l
+        const close = data.tickers[0].day.c
+        document.querySelector(".stockTitle").innerHTML = "Vanguard 500 Index Fund ETF"
+        document.querySelector(".volume").innerHTML = `Volume: ${volume}`;
+        document.querySelector(".open").innerHTML = `Open: ${open}`;
+        document.querySelector(".high").innerHTML = `High: ${high}`;
+        document.querySelector(".low").innerHTML = `Low: ${low}`;
+        document.querySelector(".closer").innerHTML = `Close: ${close}`;
     });
 }
 
 sPButtonEl.addEventListener("click", getsP);
 
+function getDowJones () {
+    fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?tickers=DIA&apiKey=97jcUfkUrkUqlBMzoexxjePUT1lnkBe4")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.tickers[0].day.v);
+        const volume = data.tickers[0].day.v
+        const open = data.tickers[0].day.o
+        const high = data.tickers[0].day.h
+        const low = data.tickers[0].day.l
+        const close = data.tickers[0].day.c
+        document.querySelector(".stockTitle").innerHTML = "SPDR Dow Jones Industrial Average ETF Trust"
+        document.querySelector(".volume").innerHTML = `Volume: ${volume}`;
+        document.querySelector(".open").innerHTML = `Open: ${open}`;
+        document.querySelector(".high").innerHTML = `High: ${high}`;
+        document.querySelector(".low").innerHTML = `Low: ${low}`;
+        document.querySelector(".closer").innerHTML = `Close: ${close}`;
+    });
+}
+
+dowJonesButtonEl.addEventListener("click", getDowJones);
+
+function getNasdaq () {
+    fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?tickers=QQQ&apiKey=97jcUfkUrkUqlBMzoexxjePUT1lnkBe4")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.tickers[0].day.v);
+        const volume = data.tickers[0].day.v
+        const open = data.tickers[0].day.o
+        const high = data.tickers[0].day.h
+        const low = data.tickers[0].day.l
+        const close = data.tickers[0].day.c
+        document.querySelector(".stockTitle").innerHTML = "Invesco QQQ Trust Series 1"
+        document.querySelector(".volume").innerHTML = `Volume: ${volume}`;
+        document.querySelector(".open").innerHTML = `Open: ${open}`;
+        document.querySelector(".high").innerHTML = `High: ${high}`;
+        document.querySelector(".low").innerHTML = `Low: ${low}`;
+        document.querySelector(".closer").innerHTML = `Close: ${close}`;
+    });
+}
+
+nasdaqButtonEl.addEventListener("click", getNasdaq);
 
 
 
@@ -47,6 +100,34 @@ sPButtonEl.addEventListener("click", getsP);
 
 
 
+
+
+
+
+
+
+
+// function getsP () {
+//     fetch("https://api.polygon.io/v2/aggs/ticker/VOO/range/1/day/2022-07-11/2022-07-11?adjusted=true&sort=asc&limit=120&apiKey=t6vX15DF8kEE3wQ4nnk9aEmGguUiai5H")
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data.results[0].v);
+//         const volume = data.results[0].v
+//         const open = data.results[0].o
+//         const high = data.results[0].h
+//         const low = data.results[0].l
+//         const close = data.results[0].c
+//         document.querySelector(".volume").innerHTML = `Volume: ${volume}`;
+//         document.querySelector(".open").innerHTML = `Open: ${open}`;
+//         document.querySelector(".high").innerHTML = `High: ${high}`;
+//         document.querySelector(".low").innerHTML = `Low: ${low}`;
+//         document.querySelector(".closer").innerHTML = `Close: ${close}`;
+//     });
+// }
+
+// sPButtonEl.addEventListener("click", getsP);
 
 
 // var getTicker = fetch("https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?apiKey=97jcUfkUrkUqlBMzoexxjePUT1lnkBe4").then(function(response) {
